f542358fe8ba8f6e01799dbc5601970476182d62
kou <kougod06@gmail.com>
2024-08-15 17:20:33 +0900

    ホームページ作成の修正２

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  skip_before_action :require_login, only: %i[new create]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:info] = '新規登録が完了しました'
+      redirect_to root_path
+    else
+      flash.now[:danger] = '新規登録失敗しました'
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
