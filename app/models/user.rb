fca6d2919ffbfd6612d7e749c124171c3584ff3a
kou <kougod06@gmail.com>
2024-08-17 16:23:54 +0900

    掲示板の一覧を作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,7 @@ class User < ApplicationRecord
   validates :password, length: { minimum: 4 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
+
+  #ユーザーが削除のときに、ユーザーにBoardも一緒に削除（一応あるあとで消すかも)
+  has_many :boards, dependent: :destroy
 end
