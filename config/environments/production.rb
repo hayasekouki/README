6bda8021637ae7422a76982f98a2449940b4b8f9
kou <kougod06@gmail.com>
2024-08-30 21:24:08 +0900

    パスワードリセットの修正４

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain: 'mennsetu-project.onrender.com',
+    domain: ENV['HOST_URL'],
     user_name:            ENV['GMAIL_USERNAME'],
     password:             ENV['GMAIL_PASSWORD'],
     authentication:       'plain',
